af37fc5c4be7bd5e3a1e9632f0a31171bca07af3
nico001 <nicovedovaldi@gmail.com>
2023-11-25 22:49:02 -0300

    comentario solo de prueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './RegistroForm.css';
 import App from './App';
+import './RegistroForm.css';
+
+//Hola!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
